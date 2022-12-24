19cab44e21bffcdfe670b1051deaa92204eda0f1
Mustafa <40084943+MosTafa2K@users.noreply.github.com>
2022-12-24 19:20:19 +0330

    Update download model.sh

--- download model.sh
+++ download model.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
 
-curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/307_YOLOv7/no-postprocess/resources.tar.gz" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
